5596ea017b98f6738b03a2d299d705f1b752330b
Ibroxim Abduolimov <abduolimovibroxim24@gmail.com>
2026-08-21 15:30:51 +0500

    Fix Flask port binding for Render

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,9 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render bergan PORT enviroment o'zgaruvchisini oladi, topilmasa 8080 ishlatadi
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
